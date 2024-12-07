086b3be277ef650eb0c59a2fcc7e4f6f30c3c864
Alex Goss <122226156+alexgossalexgoss@users.noreply.github.com>
2024-12-07 13:30:01 -0500

    template painfully applied to all notes

--- templates/scripts/templaterupdate.js
+++
@@ -1,6 +0,0 @@
-async function updateModificationDate(tp) {
-    const modificationDate = tp.file.last_modified_date("MMMM DD YYYY");
-    return `modification: ${modificationDate}`;
-}
-
-module.exports = updateModificationDate;
